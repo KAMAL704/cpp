ef26e3b9cc2ca70cca73dc687f7f6c118f09baa5
kamal <kamalbajiya15@gmail.com>
2026-06-17 07:58:13 +0530

    main

--- chapter7/homework.cpp
+++ chapter7/homework.cpp
@@ -38,27 +38,112 @@
 
 
 
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[] = {1,2,3,4,5};
+//     int small = INT_MAX;
+//     int large = INT_MIN;
+//     int sz = 5;
+//     int sum = 0;
+
+//     for(int i = 0; i<sz; i++){
+//         small = min(arr[i], small);
+//         large = max(arr[i], large);
+//     }
+    
+//     sum = small + large;
+
+
+//     cout<< small <<endl;
+//     cout<< large <<endl;
+//     cout << sum << endl;
+    
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int arr[] = {23, 43, 45, 34,32};
+//     int sz = 5;
+//     int small = INT_MAX;
+//     for(int i=0; i<sz; i++){
+//         small = min(arr[i], small);
+//     }
+//     cout<<small;
+//     cout<<endl;
+
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int c =100;
+//     int f=(9*c/5)+32;
+//     cout<<f<<"";
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int year = 2029;
+//     if(year%4 == 0){
+//         cout<<"leap yaer";
+//     }
+//     else{
+//         cout<<"not leap year";
+//     }
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int num = 153;
+//     int reverse = 0;
+//     while(num |= 0){
+//         int rem = num %10;
+//         reverse = reverse * 10 +rem;
+//         num/=10;
+//     }
+//     cout<<"reverse"<<reverse;
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int sqr(int num){
+//     int ans = 1;
+//     ans = num*num;
+//     return ans;
+// }
+
+// int main() {
+//     cout<<sqr(30)<<endl;
+    
+//     return 0;
+// }
+
+
+
+
 #include <iostream>
 using namespace std;
 
 int main() {
-    int arr[] = {1,2,3,4,5};
-    int small = INT_MAX;
-    int large = INT_MIN;
-    int sz = 5;
-    int sum = 0;
-
-    for(int i = 0; i<sz; i++){
-        small = min(arr[i], small);
-        large = max(arr[i], large);
-    }
-    
-    sum = small + large;
-
-
-    cout<< small <<endl;
-    cout<< large <<endl;
-    cout << sum << endl;
     
     return 0;
 }
